d90469ff5f658b324a8aedde25bb9e455f59fe08
llbernat <llbernat@gmail.com>
2022-01-25 14:34:51 +0100

    Created new classes Zone and Partition

---
+++ code/mt_zones.py
@@ -0,0 +1,38 @@
+# Pràctica de creació de zones electorals per algorismes genètics
+#
+# IC - MUSI 21 22 - UIB
+#
+# Alumne: Lluís Bernat Ladaria
+
+"""
+Zone design - library
+=====================
+
+Provides the objects and functions to compute a solution for
+districts assignation to almost uniform population distribution zones
+using genetic algorithms
+"""
+
+#
+# system libraries
+#
+
+import pandas as pd
+import geopandas as gpd
+import logging as log
+
+
+class Zone:
+    def __init__(self):
+        pass
+
+
+class Partition:
+    def __init__(self, data: pd.DataFrame, conn: dict, num_zones: int, pop_card: int, logger: log.Logger):
+        pass
+
+    def fit(self):
+        pass
+
+    def save_map(self, output_path):
+        pass
