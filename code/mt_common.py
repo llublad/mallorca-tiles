cdea83430a04e817a690a8c3a9c799380f2dd2dc
llbernat <llbernat@gmail.com>
2022-01-25 20:54:42 +0100

    Defined the two nested dict keys for neighbours ranking

---
+++ code/mt_common.py
@@ -0,0 +1,18 @@
+# Pràctica de creació de zones electorals per algorismes genètics
+#
+# IC - MUSI 21 22 - UIB
+#
+# Alumne: Lluís Bernat Ladaria
+
+"""
+Zone design - library
+=====================
+
+Provides the objects and functions to compute a solution for
+districts assignation to almost uniform population distribution zones
+using genetic algorithms
+"""
+
+
+DICT_DISTRICT_NEIGHBOURS_ID_LIST = 'NEIGHBOURS_ID_LIST'
+DICT_DISTRICT_NEIGHBOURS_SLICE_LIST = 'NEIGHBOURS_PROB_LIST'
