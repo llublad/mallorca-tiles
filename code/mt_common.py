c14858d60875139b6eebdd537c464f4ef5522b70
llbernat <llbernat@gmail.com>
2022-01-26 14:24:46 +0100

    Added basic Partition input checking

--- code/mt_common.py
+++ code/mt_common.py
@@ -14,5 +14,17 @@ using genetic algorithms
 """
 
 
+#
+# some constants
+#
 DICT_DISTRICT_NEIGHBOURS_ID_LIST = 'NEIGHBOURS_ID_LIST'
 DICT_DISTRICT_NEIGHBOURS_SLICE_LIST = 'NEIGHBOURS_PROB_LIST'
+
+
+#
+# messages
+#
+
+LOG_ERROR_DATA_SYNTAX = "Some input data has incorrect syntax"
+LOG_ERROR_NUM_ZONES = "'num_zones' must be an integer between 1 and data cardinality"
+LOG_ERROR_POP_CARD = "'pop_card' must be an even integer"
--- code/mt_zones.py
+++ code/mt_zones.py
@@ -35,9 +35,61 @@ class Zone:
         pass
 
 
+def check_data(data):
+
+    is_correct = True
+
+    return is_correct
+
+
+def check_conn(conn):
+
+    is_correct = True
+
+    return is_correct
+
+
+def check_num_zones(num_zones: int, data: pd.DataFrame) -> bool:
+    # check if num_zones is between 1 and data cardinality
+    if not(type(num_zones) is int and type(data) is pd.DataFrame):
+        raise TypeError
+
+    is_correct = (num_zones >= 1) and (num_zones <= len(data))
+
+    if not is_correct:
+        log.error(our.LOG_ERROR_NUM_ZONES)
+        raise ValueError(our.LOG_ERROR_NUM_ZONES)
+
+    return is_correct
+
+
+def check_pop_card(pop_card: int) -> bool:
+    # check if pop_card is even
+    if not type(pop_card) is int:
+        raise TypeError
+
+    is_correct = (pop_card % 2 == 0)
+
+    if not is_correct:
+        log.error(our.LOG_ERROR_POP_CARD)
+        raise ValueError
+
+    return is_correct
+
+
 class Partition:
     def __init__(self, data: pd.DataFrame, conn: dict, num_zones: int, pop_card: int, logger: log.Logger):
-        pass
+
+        if check_data(data) and check_conn(conn) and \
+                check_num_zones(num_zones, data) and check_pop_card(pop_card):
+            self.data = data
+            self.conn = conn
+            self.num_zones = num_zones
+            self.pop_card = pop_card
+        else:
+            logger.error(our.LOG_ERROR_DATA_SYNTAX)
+
+        return
 
     def fit(self):
         pass
