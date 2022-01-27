83bf999d4feabd0102cef2baf80f16241c3df722
llbernat <llbernat@gmail.com>
2022-01-27 21:08:42 +0100

    Added more info and error messages

--- code/mt_common.py
+++ code/mt_common.py
@@ -13,7 +13,6 @@ districts assignation to almost uniform population distribution zones
 using genetic algorithms
 """
 
-
 #
 # some constants
 #
@@ -23,17 +22,24 @@ DATA_VALUE_FIELD = 'VALUE'
 DICT_DISTRICT_NEIGHBOURS_CODE_LIST = 'NEIGHBOURS_CODE_LIST'
 DICT_DISTRICT_NEIGHBOURS_COST_LIST = 'NEIGHBOURS_COST_LIST'
 
-
 #
 # messages
 #
 
+MG_INFO_COMPUTING_VALID_AREA = \
+    "Computing the valid zone centroid map"
+MG_INFO_PARTITION_DESIGNER_INIT = \
+    "Created a new PartitionDesigner object to accommodate {} districts " \
+    "into {} zones. Using an initial population of {} partition instances"
 MG_INFO_PARTITION_INIT = \
-    "Created a new PartitionDesigner object with zones={} and population={}"
+    "Created a new Partition object to accommodate {} districts " \
+    "into {} zones"
 MG_ERROR_DATA = \
     "'data' must be a DataFrame with 'CODE' and 'VALUE' fields"
 MG_ERROR_CONN = \
     "'conn' must be a dictionary with a key entry for each 'data' 'CODE' value"
+MG_ERROR_VALID_AREA_MAP = \
+    "'valid_area' must be a geometric object representing the valid zone centroids area"
 MG_ERROR_NUM_ZONES = \
     "'num_zones' must be an integer between 1 and data cardinality"
 MG_ERROR_POP_CARD = \
