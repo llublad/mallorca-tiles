ddc0f9dfab7bba5a7251ddccaa86c363684556a2
llbernat <llbernat@gmail.com>
2022-02-08 20:28:16 +0100

    Added evaluate() and mutate() declarations

--- code/mt_Partition.py
+++ code/mt_Partition.py
@@ -41,23 +41,33 @@ class Partition:
                  valid_area: BaseGeometry,
                  num_zones: int, logger: log.Logger):
         # create an instance of the partition genotype
+        #
+
+        # save parameters
         self.data = data
         self.conn = conn
         self.valid_area = valid_area
         self.num_zones = num_zones
         self.logger = logger
 
-        [self.x_min, self.y_min, self.x_max, self.y_max] = \
-            self.valid_area.bounds
-
+        # calculate number of districts to fit
         num_districts = len(data)
         self.num_districts = num_districts
 
+        # say hello
         self.logger.debug(
             our.MG_INFO_PARTITION_INIT.format(self.num_districts, self.num_zones))
 
+        # calc cartesian boundaris (the smaller rectangle that cotains the map)
+        [self.x_min, self.y_min, self.x_max, self.y_max] = \
+            self.valid_area.bounds
+
+        # our list of centroids
         self.genotype = list()
         self._generate_genotype()
+
+        # our solution score
+        self.score = None
 
         pass
 
@@ -69,20 +79,70 @@ class Partition:
         if len(self.genotype) > 0:
             raise OverflowError(our.MG_DEBUG_INTERNAL_ERROR)
 
-        i = 0
-        while i < self.num_zones:
-            # generate a random (x, y) point into map cartesian bounds
-            x = random.uniform(self.x_min, self.x_max)
-            y = random.uniform(self.y_min, self.y_max)
-            p = Point(x, y)
-            # the point must be into the region of interest
-            # and of course must be unique to this instance
-            if self.valid_area.contains(p) and p not in self.genotype:
-                i += 1
-                self.genotype.append(p)
+        for i in range(self.num_zones):
+            p = self._generate_new_valid_point()
+            self.genotype.append(p)
+            i += 1
 
         pass
 
-    def fit(self):
+    def _generate_new_valid_point(self):
+        # return a new valid point
+        # (a no repeated point that is contained
+        # into the study region)
+
+        p = self._generate_point()
+
+        # the point must be into the study region
+        # and of course must be unique to this instance
+        while not self.valid_area.contains(p) or p in self.genotype:
+            # get a new point
+            # while point is not valid or point is not unique
+            p = self._generate_point()
+
+        return p
+
+    def _generate_point(self):
+        # return a point into rectangular map boundaries
+        # the point can be invalid
+        # (to be valid must be contained
+        # into the study region)
+
+        # generate a random (x, y) point into map cartesian bounds
+        x = random.uniform(self.x_min, self.x_max)
+        y = random.uniform(self.y_min, self.y_max)
+        p = Point(x, y)
+
+        return p
+
+    def compose_partition(self):
+
+        # TODO
+
+        pass
+
+    def evaluate(self):
+
+        # TODO
+
+        pass
+
+    def mutate(self, prob: float):
+        # with probability 'prob'
+        # apply a random mutation
+
+        # TODO
+
+        pass
+
+    def plot(self):
+
+        # TODO
+
+        pass
+
+    def savefig(self, path: str):
+
+        # TODO
 
         pass
