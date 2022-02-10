d5b3d40e9b4f836f84c95cff5a4fc7b765270836
llbernat <llbernat@gmail.com>
2022-02-10 18:30:16 +0100

    Added new info message about partition restoration

--- code/mt_common.py
+++ code/mt_common.py
@@ -18,16 +18,22 @@ using genetic algorithms
 #
 EPS_DIV0 = 1e-6  # A very small number to avoid divide by zero errors at some geo-calcs
 
-GA_MAX_ITERATIONS = 1000
-GA_NOIMPROV_ITERATIONS = 200
+GA_MAX_ITERATIONS = 10
+GA_NOIMPROV_ITERATIONS = 2
 GA_CROSSOVER_PROB = 0.95
 GA_MUTATION_PROB = 0.001
 
-DATA_CODE_FIELD = 'CODE'
-DATA_VALUE_FIELD = 'VALUE'
+# what are the interesting fields in loaded from file panda DataFrame
+PD_DATA_CODE_FIELD = 'CODE'
+PD_DATA_VALUE_FIELD = 'VALUE'
+
+# what are containig the columns in the list of data
+LIST_DATA_CODE_COL = 0
+LIST_DATA_VALUE_COL = 1
 
 DICT_DISTRICT_NEIGHBOURS_CODE_LIST = 'NEIGHBOURS_CODE_LIST'
 DICT_DISTRICT_NEIGHBOURS_COST_LIST = 'NEIGHBOURS_COST_LIST'
+DICT_DISTRICT_CENTROID_POINT = 'CENTROID'
 
 #
 # messages
@@ -37,19 +43,21 @@ MG_DEBUG_INTERNAL_ERROR = \
     "Please debug this internal error"
 
 MG_INFO_COMPUTING_VALID_AREA = \
-    "Computing the valid zone centroid map"
+    "Computing the feasible zone centers region map"
 MG_INFO_PARTITION_DESIGNER_INIT = \
     "Created a new PartitionDesigner object to accommodate {} districts " \
     "into {} zones. Using an initial population of {} partition instances"
 MG_INFO_PARTITION_INIT = \
     "Created a new Partition object to accommodate {} districts " \
     "into {} zones"
+MG_INFO_PARTITIONS_RESTORED = \
+    "All partition objects have been restored (stats zeroed and zone list deleted)"
 MG_ERROR_DATA = \
-    f"'data' must be a DataFrame with '{DATA_CODE_FIELD}' and '{DATA_VALUE_FIELD}' fields"
+    f"'data' must be a DataFrame with '{PD_DATA_CODE_FIELD}' and '{PD_DATA_VALUE_FIELD}' fields"
 MG_ERROR_CONN = \
-    f"'conn' must be a dictionary with a key entry for each 'data' '{DATA_CODE_FIELD}' value"
+    f"'geodata' must be a dictionary with a key entry for each 'data' '{PD_DATA_CODE_FIELD}' value"
 MG_ERROR_VALID_AREA_MAP = \
-    "'valid_area' must be a geometric object representing the valid zone centroids area"
+    "'valid_area' must be a geometric object representing the valid zone centers area"
 MG_ERROR_NUM_ZONES = \
     "'num_zones' must be an integer between 1 and data cardinality"
 MG_ERROR_POP_CARD = \
